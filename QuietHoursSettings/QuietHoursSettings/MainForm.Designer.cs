31ff5106390330be2f6d1d511b349ff35ef8d1a7
TwoUnderscorez <ron.yutkin@gmail.com>
2017-12-22 09:14:10 +0200

    add gui

--- QuietHoursSettings/QuietHoursSettings/MainForm.Designer.cs
+++ QuietHoursSettings/QuietHoursSettings/MainForm.Designer.cs
@@ -32,10 +32,21 @@
             this.metroStyleManager = new MetroFramework.Components.MetroStyleManager(this.components);
             this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
             this.settings_tab = new MetroFramework.Controls.MetroTabPage();
+            this.end_min_txt = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.end_hours_txt = new MetroFramework.Controls.MetroTextBox();
+            this.start_min_txt = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.start_hours_txt = new MetroFramework.Controls.MetroTextBox();
+            this.exit_btn = new MetroFramework.Controls.MetroButton();
+            this.apply_btn = new MetroFramework.Controls.MetroButton();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.about_tab = new MetroFramework.Controls.MetroTabPage();
             this.about_txt = new MetroFramework.Controls.MetroTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager)).BeginInit();
             this.metroTabControl1.SuspendLayout();
+            this.settings_tab.SuspendLayout();
             this.about_tab.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,6 +67,16 @@
             // 
             // settings_tab
             // 
+            this.settings_tab.Controls.Add(this.end_min_txt);
+            this.settings_tab.Controls.Add(this.metroLabel4);
+            this.settings_tab.Controls.Add(this.end_hours_txt);
+            this.settings_tab.Controls.Add(this.start_min_txt);
+            this.settings_tab.Controls.Add(this.metroLabel3);
+            this.settings_tab.Controls.Add(this.start_hours_txt);
+            this.settings_tab.Controls.Add(this.exit_btn);
+            this.settings_tab.Controls.Add(this.apply_btn);
+            this.settings_tab.Controls.Add(this.metroLabel2);
+            this.settings_tab.Controls.Add(this.metroLabel1);
             this.settings_tab.HorizontalScrollbarBarColor = true;
             this.settings_tab.Location = new System.Drawing.Point(4, 35);
             this.settings_tab.Name = "settings_tab";
@@ -63,6 +84,92 @@
             this.settings_tab.TabIndex = 0;
             this.settings_tab.Text = "Settings";
             this.settings_tab.VerticalScrollbarBarColor = true;
+            // 
+            // end_min_txt
+            // 
+            this.end_min_txt.Location = new System.Drawing.Point(122, 35);
+            this.end_min_txt.Name = "end_min_txt";
+            this.end_min_txt.Size = new System.Drawing.Size(26, 23);
+            this.end_min_txt.TabIndex = 11;
+            this.end_min_txt.TextChanged += new System.EventHandler(this.txts_TextChanged);
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(110, 35);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(12, 19);
+            this.metroLabel4.TabIndex = 10;
+            this.metroLabel4.Text = ":";
+            // 
+            // end_hours_txt
+            // 
+            this.end_hours_txt.Location = new System.Drawing.Point(83, 35);
+            this.end_hours_txt.Name = "end_hours_txt";
+            this.end_hours_txt.Size = new System.Drawing.Size(26, 23);
+            this.end_hours_txt.TabIndex = 9;
+            this.end_hours_txt.TextChanged += new System.EventHandler(this.txts_TextChanged);
+            // 
+            // start_min_txt
+            // 
+            this.start_min_txt.Location = new System.Drawing.Point(122, 4);
+            this.start_min_txt.Name = "start_min_txt";
+            this.start_min_txt.Size = new System.Drawing.Size(26, 23);
+            this.start_min_txt.TabIndex = 8;
+            this.start_min_txt.TextChanged += new System.EventHandler(this.txts_TextChanged);
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(110, 4);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(12, 19);
+            this.metroLabel3.TabIndex = 7;
+            this.metroLabel3.Text = ":";
+            // 
+            // start_hours_txt
+            // 
+            this.start_hours_txt.Location = new System.Drawing.Point(83, 4);
+            this.start_hours_txt.Name = "start_hours_txt";
+            this.start_hours_txt.Size = new System.Drawing.Size(26, 23);
+            this.start_hours_txt.TabIndex = 6;
+            this.start_hours_txt.TextChanged += new System.EventHandler(this.txts_TextChanged);
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.Location = new System.Drawing.Point(301, 172);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(75, 23);
+            this.exit_btn.TabIndex = 5;
+            this.exit_btn.Text = "Exit";
+            // 
+            // apply_btn
+            // 
+            this.apply_btn.Highlight = true;
+            this.apply_btn.Location = new System.Drawing.Point(382, 172);
+            this.apply_btn.Name = "apply_btn";
+            this.apply_btn.Size = new System.Drawing.Size(75, 23);
+            this.apply_btn.Style = MetroFramework.MetroColorStyle.Blue;
+            this.apply_btn.TabIndex = 4;
+            this.apply_btn.Text = "Apply";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(4, 35);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(64, 19);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "End Time";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(4, 4);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(73, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "Start Time:";
             // 
             // about_tab
             // 
@@ -97,6 +204,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager)).EndInit();
             this.metroTabControl1.ResumeLayout(false);
+            this.settings_tab.ResumeLayout(false);
+            this.settings_tab.PerformLayout();
             this.about_tab.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -109,6 +218,16 @@
         private MetroFramework.Controls.MetroTabPage settings_tab;
         private MetroFramework.Controls.MetroTabPage about_tab;
         private MetroFramework.Controls.MetroTextBox about_txt;
+        private MetroFramework.Controls.MetroTextBox end_min_txt;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroTextBox end_hours_txt;
+        private MetroFramework.Controls.MetroTextBox start_min_txt;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroTextBox start_hours_txt;
+        private MetroFramework.Controls.MetroButton exit_btn;
+        private MetroFramework.Controls.MetroButton apply_btn;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
     }
 }
 
--- QuietHoursSettings/QuietHoursSettings/MainForm.cs
+++ QuietHoursSettings/QuietHoursSettings/MainForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MetroFramework.Forms;
+using MetroFramework.Controls;
 
 namespace QuietHoursSettings
 {
@@ -22,6 +23,29 @@ namespace QuietHoursSettings
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void txts_TextChanged(object sender, EventArgs e)
+        {
+            MetroTextBox txt = (MetroTextBox)sender;
+            if(!IsDigitsOnly(txt.Text))
+            {
+                MessageBox.Show("Please use numbers only!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txt.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// Checks whether a string contains only numbers
+        /// </summary>
+        /// <param name="str">String to check</param>
+        /// <returns></returns>
+        bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+                if (c < '0' || c > '9')
+                    return false;
+            return true;
         }
     }
 }
